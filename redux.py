82295e5459c9f8411bb8d8ac0b2a442c340eb895
Nanne Wielinga <nanne@mycel.nl>
2019-08-07 06:58:37 +0200

    Initial version

---
+++ redux.py
@@ -0,0 +1,25 @@
+import subprocess
+import io
+from textx import metamodel_from_file, get_children_of_type
+from jinja2 import Template
+
+redux_mm = metamodel_from_file('languages/redux/redux.tx')
+redux_model = redux_mm.model_from_file('apps/counter/CountApp.rdx')
+
+program = ""
+
+
+actions = get_children_of_type('Action', redux_model)
+reducers = get_children_of_type('Reducer', redux_model)
+# reducers = get_children_of_type('Reducer', redux_model)
+
+rendered = Template(open('mcrl2.j2').read()) \
+    .render(actions=actions, reducers=reducers)
+
+print(rendered)
+
+# generated = rendered.encode('utf-8')
+# p = subprocess.run(["mcrl22lps"], stdout=subprocess.PIPE, input=generated,)
+
+# print(p.stdout.decode('utf-8'))
+
